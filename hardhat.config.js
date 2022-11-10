7e4e1adc7ed43d11e0c35ddf9f067db1fd26e202
bigbyrd69 <kumarsonal9955@gmail.com>
2022-11-10 13:13:06 +0530

    message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("deploy", "Deploy the smart contracts", async(taskArgs, hre) => {
+
+  const Artwork = await hre.ethers.getContractFactory("Artwork");
+  const artwork = await Artwork.deploy("Artwork Contract", "ART");
+
+  await artwork.deployed();
+
+  await hre.run("verify:verify", {
+    address: artwork.address,
+    constructorArguments: [
+      "Artwork Contract",
+      "ART"
+    ]
+  })
+
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
